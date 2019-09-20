92707778758e071bd88b9a3ff10d26e386d45853
suskin <813571+suskin@users.noreply.github.com>
2019-09-19 17:01:12 -0700

    stacks scenario: add initial setup script
    We want to do some of the boilerplate for users when they open the
    scenario, so this changeset adds a script to do it for people in the
    foreground.
    
    Signed-off-by: suskin <813571+suskin@users.noreply.github.com>

---
+++ stacks/assets/initial-setup.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# Install Crossplane CLI
+RELEASE=release-0.1 && curl -sL https://raw.githubusercontent.com/crossplaneio/crossplane-cli/"${RELEASE}"/bootstrap.sh | RELEASE=${RELEASE} bash
+
+# Finish setting up helm
+helm init
+
+# Install Crossplane
+helm repo add crossplane-alpha https://charts.crossplane.io/alpha
+helm install --name crossplane --namespace crossplane-system crossplane-alpha/crossplane
+
+# Initialize the environment
+
+# Install go
+snap install --classic go
+
+# Set up go
+export GOPATH=$HOME/go
+mkdir -p ${GOPATH}/src
+
+
+# Install kubebuilder
+
+os=$(go env GOOS)
+arch=$(go env GOARCH)
+
+# download kubebuilder and extract it to tmp
+curl -sL https://go.kubebuilder.io/dl/2.0.0/${os}/${arch} | tar -xz -C /tmp/
+
+# move to a long-term location and put it on your path
+# (you'll need to set the KUBEBUILDER_ASSETS env var if you put it somewhere else)
+sudo mv /tmp/kubebuilder_2.0.0_${os}_${arch} /usr/local/kubebuilder
+export PATH=$PATH:/usr/local/kubebuilder/bin
+
+# Initialize project
+
+PROJECT_DIR=${GOPATH}/src/helloworld
+mkdir -p ${PROJECT_DIR}
+cd ${PROJECT_DIR}
+git init
+
+# Initialize project with kubebuilder
+GO111MODULE=on kubebuilder init --domain helloworld.stacks.crossplane.io
